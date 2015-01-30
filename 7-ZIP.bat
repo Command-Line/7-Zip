1b8b2d1ec81ddd4a760a3dba8663a71029fa300f
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2015-01-30 16:16:19 +0100

    Create 7-ZIP.bat

---
+++ 7-ZIP.bat
@@ -0,0 +1,12 @@
+@echo off
+cls
+title Command Line (CL) - 7-Zip
+
+echo Command Line (CL) - 7-Zip Program
+echo (C) Copyright 2014-present Suriyaa Kudo
+echo.
+
+ver
+:cmd
+cd 7-zip
+start 7zFM.exe
